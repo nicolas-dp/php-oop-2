0f41833e4d2148ea7e190493357e24e719ea7c09
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-05-26 14:39:35 +0200

    Initial Page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!-- L'e-commerce vende prodotti per gli animali.
+I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
+Il pagamento avviene con la carta di credito, che non deve essere scaduta. -->
+
+<?php 
+
+class Negozio{
+
+    
+}
